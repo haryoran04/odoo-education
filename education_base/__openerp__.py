b42ce0b0784978a1d96d6812aafe28807dcd44bc
Mantavya Gajjar <mantavyagajjar@mga-air-11-wifi.odoo.co.in>
2016-07-29 18:53:57 +0530

    [INIT]: init modules with description, ready to launch on apps.odoo.com

---
+++ education_base/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Education Management",
+
+    'summary': "Manage Student Attedence, Exams and Results",
+
+    'description': """Education Management Base System""",
+
+    'author': "Odoo Education",
+    'website': "http://www.odooeducation.com",
+
+    'category': 'Education',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+    'images': [
+        'static/description/index.png',
+    ],
+    'license':'Other proprietary',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ education_library/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library Management",
+
+    'summary': "Manage Books, Issue and Return",
+
+    'description': """
+        Library Management System""",
+
+    'author': "Odoo Education",
+    'website': "http://www.odooeducation.com",
+
+
+    'category': 'Education',
+    'version': '0.1',
+
+    'depends': ['education_base'],
+
+    'data': [
+      
+    ],
+    'images': [
+        '/education_base/static/description/board.jpg'
+    ],
+    
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
